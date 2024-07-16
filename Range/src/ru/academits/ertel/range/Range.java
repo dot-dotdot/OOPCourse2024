a6a85c30a529d07358ea744cb9f6ce8c386454ae
dot-dotdot <ilyaehrtel@yandex.ru>
2024-07-16 16:11:34 +0700

    Added methods for set operations on ranges. Added tests to main.

--- Range/src/ru/academits/ertel/range/Range.java
+++ Range/src/ru/academits/ertel/range/Range.java
@@ -1,4 +1,82 @@
 package ru.academits.ertel.range;
 
 public class Range {
+    private double from;
+    private double to;
+
+    public Range(double from, double to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    public double getFrom() {
+        return from;
+    }
+
+    public void setFrom(double from) {
+        this.from = from;
+    }
+
+    public double getTo() {
+        return to;
+    }
+
+    public void setTo(double to) {
+        this.to = to;
+    }
+
+    public double getLength() {
+        return to - from;
+    }
+
+    public boolean isInside(double number) {
+        return number >= from && number <= to;
+    }
+
+    public Range getIntersection(Range range) {
+        if (this.from == range.from && this.to == range.to) {
+            return new Range(this.from, this.to);
+        }
+
+        if (range.from >= this.to || this.from >= range.to) {
+            return null;
+        }
+
+        double intersectionFrom = Math.max(this.from, range.from);
+        double intersectionTo = Math.min(this.to, range.to);
+
+        return new Range(intersectionFrom, intersectionTo);
+    }
+
+    public Range[] getUnion(Range range) {
+        if (this.from == range.from && this.to == range.to) {
+            return new Range[] {new Range(this.from, this.to)};
+        }
+
+        if (range.from > this.to || this.from > range.to) {
+            return new Range[] {new Range(this.from, this.to), new Range(range.from, range.to)};
+        }
+
+        double unionFrom = Math.min(this.from, range.from);
+        double unionTo = Math.max(this.to, range.to);
+
+        return new Range[] {new Range(unionFrom, unionTo)};
+    }
+
+    public Range[] getComplement(Range range) {
+        if (this.from >= range.from && this.to <= range.to) {
+            return new Range[0];
+        }
+
+        if (range.from >= this.to || this.from >= range.to) {
+            return new Range[] {new Range(this.from, this.to)};
+        }
+
+        if (this.from < range.from && this.to > range.to) {
+            return new Range[] {new Range(this.from, range.from), new Range(range.to, this.to)};
+        }
+
+        return (this.from < range.to && this.to > range.to) ?
+                new Range[] {new Range(range.to, this.to)} : new Range[] {new Range(this.from, range.from)};
+    }
 }
--- Range/src/ru/academits/ertel/range_main/Main.java
+++ Range/src/ru/academits/ertel/range_main/Main.java
@@ -1,7 +1,48 @@
 package ru.academits.ertel.range_main;
 
+import ru.academits.ertel.range.Range;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        Range range1 = new Range(4, 8);
+        Range range2 = new Range(2, 6);
+
+        Range intersection = range1.getIntersection(range2);
+
+        System.out.println("Пересечение:");
+
+        if (intersection == null) {
+            System.out.println("Нет пересечений");
+        } else {
+            System.out.println(intersection.getFrom() + ", " + intersection.getTo());
+        }
+
+        System.out.println();
+
+        Range[] union = range1.getUnion(range2);
+
+        System.out.println("Объединение:");
+
+        for (Range range : union) {
+            double from = range.getFrom();
+            double to = range.getTo();
+            System.out.println(from + ", " + to);
+        }
+
+        System.out.println();
+
+        Range[] complement = range1.getComplement(range2);
+
+        System.out.println("Разность:");
+
+        if (complement.length == 0) {
+            System.out.println("Интервал находится внутри вычитаемого интервала.");
+        } else {
+            for (Range range : complement) {
+                double from = range.getFrom();
+                double to = range.getTo();
+                System.out.println(from + ", " + to);
+            }
+        }
     }
 }
